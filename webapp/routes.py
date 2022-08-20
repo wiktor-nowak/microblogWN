adea3e3e50fb9ed618d160bf02250b6f8aebeb25
DESKTOP-DPTPDCH\nowak <nowak.wiktor95@gmail.com>
2022-08-20 16:33:23 +0200

    no message

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,7 +1,25 @@
+from flask import render_template, flash, redirect
 from webapp import app
-
+from webapp.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello boy! -_-"
+    user = {'username': 'Wiktor'}
+    posts = [
+        {
+            'author': {'username': 'Karolinka'},
+            'body': 'Kupiłam nowy telefon i będę robić świetne zdjęcia!'
+        }
+    ]
+    return render_template('index.html', title='Hello App', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, '
+              f'remember me = {form.remember_me.data}')
+        return redirect('/index')
+    return render_template('login.html', title = "Sign In", form=form)
