85088f0e783ea9d6da42b26932dda62a79f80eb9
DESKTOP-DPTPDCH\nowak <nowak.wiktor95@gmail.com>
2022-08-21 13:20:38 +0200

    Changes in DB: Added "post" table and relations

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from webapp import db
 
 class User(db.Model):
@@ -5,6 +6,20 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    # Pierwszy argoment to nazwa klasy budującej tabelę SQL,
+    # do której się odnosi relacja ( a więc duże P )
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(300))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # user.id w tym przypadku user to tabela w bazie danych zbudowanej
+    # przez Alchemie na bazie klasy User przez duże U... O_O
+
+    def __repr__(self):
+        return f'<Post {self.body}>'
