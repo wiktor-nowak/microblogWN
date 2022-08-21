af60428ca19496e4f9adea472476d68eb20b94e0
DESKTOP-DPTPDCH\nowak <nowak.wiktor95@gmail.com>
2022-08-21 12:35:37 +0200

    Adding DB through SQLAlchemy and Migration based on Alembic

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-hard-to-break=haslo'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db') #sqlite:///microblog/app.db ???
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # TODO 1: Dopytać Kubę o szczegóły >os.environ.get...<
--- webapp/routes.py
+++ webapp/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from webapp import app
 from webapp.forms import LoginForm
 
@@ -21,5 +21,5 @@ def login():
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data}, '
               f'remember me = {form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title = "Sign In", form=form)
